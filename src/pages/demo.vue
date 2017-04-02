81c30e1a942518a744b378e15b87b2b84cb18bc6
Clark Du <duxin@cfilmcloud.com>
2017-04-02 18:41:29 +0800

    add demo for [radio, checkbox, carousel]

--- src/pages/demo.vue
+++ src/pages/demo.vue
@@ -1,41 +1,81 @@
 <template>
   <div class="content" v-if="loggedUser">
-    <el-card>
-      Element 卡片演示
-    </el-card>
-    <img :src="loggedUser.picture"/>
-    <p>Hi {{ loggedUser.email }}!</p>
-    <p>This is a super secure page! Try loading this page again using the incognito/private mode of your browser.</p>
-    <div style="margin: 15px 0;"></div>
-    <el-row>
-      <el-col :span="6">
-        <el-select v-model="selectval" placeholder="请选择">
-          <el-option
-            v-for="item in food"
-            :key="item.label"
-            :label="item.label"
-            :value="item.value"
-            :disabled="item.disabled">
-          </el-option>
-        </el-select>
-      </el-col>
-      <el-col :span="6">
-        <el-button>Default Button</el-button>
-        <el-button type="primary">Primary Button</el-button>
-        <el-button type="text">Text Button</el-button>
-      </el-col>
-      <el-col :span="6">
-        <el-input-number v-model="num" :min="1" :max="10"></el-input-number>
-      </el-col>
-      <el-col :span="6">
-        <el-radio-group v-model="city">
-          <el-radio-button label="1">上海</el-radio-button>
-          <el-radio-button label="2" :disabled="true">北京</el-radio-button>
-          <el-radio-button label="3">广州</el-radio-button>
-          <el-radio-button label="4">深圳</el-radio-button>
-        </el-radio-group>
-      </el-col>
-    </el-row>
+    <el-carousel :interval="50000" height="580px" indicator-position="outside">
+      <el-carousel-item>
+        <el-row>
+          <el-col :offset="4" :span="16">
+            <el-card>
+              Element 下拉框, 按钮, 计数器, 单选框演示
+            </el-card>
+          </el-col>
+        </el-row>
+        <img :src="loggedUser.picture"/>
+        <p>Hi {{ loggedUser.email }}!</p>
+        <p>This is a super secure page! Try loading this page again using the incognito/private mode of your browser.</p>
+        <div style="margin: 15px 0;"></div>
+        <el-row>
+          <el-col :span="6">
+            <el-select v-model="selectval" placeholder="请选择">
+              <el-option
+                v-for="item in food"
+                :key="item.label"
+                :label="item.label"
+                :value="item.value"
+                :disabled="item.disabled">
+              </el-option>
+            </el-select>
+          </el-col>
+          <el-col :span="6">
+            <el-button>Default Button</el-button>
+            <el-button type="primary">Primary Button</el-button>
+            <el-button type="text">Text Button</el-button>
+          </el-col>
+          <el-col :span="6">
+            <el-input-number v-model="num" :min="1" :max="10"></el-input-number>
+          </el-col>
+          <el-col :span="6">
+            <el-radio-group v-model="city">
+              <el-radio-button label="1">上海</el-radio-button>
+              <el-radio-button label="2" :disabled="true">北京</el-radio-button>
+              <el-radio-button label="3">广州</el-radio-button>
+              <el-radio-button label="4">深圳</el-radio-button>
+            </el-radio-group>
+          </el-col>
+        </el-row>
+      </el-carousel-item>
+      <el-carousel-item>
+        <el-row>
+          <el-col :offset="4" :span="16">
+            <el-card>
+              Element 单选框, 多选框演示
+            </el-card>
+          </el-col>
+        </el-row>
+        <div style="margin: 15px 0;"></div>
+        <el-row>
+          <el-col :span="6">
+            <el-radio-group v-model="province">
+              <el-radio :label="3">辽宁</el-radio>
+              <el-radio disabled :label="6">浙江</el-radio>
+              <el-radio :label="9">台湾</el-radio>
+            </el-radio-group>
+          </el-col>
+          <el-col :span="12">
+            <el-checkbox-group v-model="district">
+              <el-checkbox label="2">中山区</el-checkbox>
+              <el-checkbox label="4">东城区</el-checkbox>
+              <el-checkbox label="6">松山区</el-checkbox>
+              <el-checkbox label="8" disabled>和平区</el-checkbox>
+              <el-checkbox label="10" disabled>华盛顿哥伦比亚特区</el-checkbox>
+            </el-checkbox-group>
+          </el-col>
+          <el-col :span="6">
+          </el-col>
+          <el-col :span="6">
+          </el-col>
+        </el-row>
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
@@ -49,6 +89,8 @@ export default {
     return {
       num: '1',
       city: '3',
+      province: 6,
+      district: ['2', '8'],
       food: [{
         value: '选项1',
         label: '黄金糕'
