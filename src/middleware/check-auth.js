350cb386e6b2b0ba83fb0617b6e9d0dc8ddfa74b
Clark Du <duxin@cfilmcloud.com>
2017-04-06 12:15:12 +0800

    add hare logo

--- src/middleware/check-auth.js
+++ src/middleware/check-auth.js
@@ -2,7 +2,7 @@ export default function ({ isServer, store, req }) {
   const loggedUser = {
     username: 'Clark',
     email: 'clark.duxin@gmail.com',
-    picture: require('~assets/img/' + ((req || {}).imageName || 'hare.jpg'))
+    picture: require('~assets/img/' + ((req || {}).imageName || 'hare-logo-blue.svg'))
   }
   store.commit('SET_USER', loggedUser)
 }
