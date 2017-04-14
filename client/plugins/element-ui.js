83aaea97f3773c2ff524274c3f4b884c750028b3
Clark Du <duxin@cfilmcloud.com>
2017-04-14 11:58:07 +0800

    change elm-ui language to chinese

--- client/plugins/element-ui.js
+++ client/plugins/element-ui.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import Element from 'element-ui/lib/element-ui.common'
-import locale from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale/lang/zh-CN'
 Vue.use(Element, { locale })
