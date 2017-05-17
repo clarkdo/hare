43f12664f03ad0b0535b1990cb385ee7f9384949
clarkdo <clark.duxin@gmail.com>
2017-05-17 21:49:03 +0800

    refactor styles

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -27,32 +27,6 @@
             </el-col>
           </el-row>
         </el-card>
-        <el-card v-else>
-          <el-form :model="user" :rules="rules" ref="user">
-            <el-form-item prop="userName" :rules="[{ required: true, message: '用户名不能为空'}]">
-              <el-col :span="24">
-                <el-input v-model="user.userName" placeholder="用户名"></el-input>
-              </el-col>
-            </el-form-item>
-            <el-form-item prop="password" :rules="[{ required: true, message: '密码不能为空'}]">
-              <el-col :span="24">
-                <el-input v-model="user.password" type="password" placeholder="密码"></el-input>
-              </el-col>
-            </el-form-item>
-            <el-form-item prop="captcha" :rules="[{ required: true, message: '验证码不能为空'}]">
-              <el-col :span="9">
-                <el-input v-model="user.captcha" placeholder="验证码"></el-input>
-              </el-col>
-              <el-col :offset="2" :span="12" v-html="captchaSvg" class="captcha">
-              </el-col>
-            </el-form-item>
-            <el-row>
-              <el-col :span="24">
-                <el-button type="primary" class="login" :loading="logining" @click="login">登录</el-button>
-              </el-col>
-            </el-row>
-          </el-form>
-        </el-card>
       </div>
     </div>
   </div>
@@ -77,12 +51,13 @@
     color: #fff;
     margin-bottom: 130px;
     .container {
+      padding: 0 20px;
       position: relative;
     }
     img {
       position: absolute;
       top: 15px;
-      right: -10px;
+      right: 0px;
     }
   }
   .banner-sky {
@@ -119,81 +94,6 @@
       padding-left: 3px;
     }
   }
-  .cards {
-    margin: 0 auto 110px;
-    width: 1140px;
-    .container {
-      padding: 0;
-      margin: 0 -11px;
-      width: auto;
-    }
-    li {
-      width: 33.33333%;
-      padding: 0 19px;
-      box-sizing: border-box;
-      float: left;
-      list-style: none;
-    }
-    img {
-      width: 160px;
-      height: 120px;
-    }
-  }
-  .card {
-    height: 430px;
-    width: 100%;
-    background:#ffffff;
-    border:1px solid #eaeefb;
-    border-radius:5px;
-    box-sizing: border-box;
-    text-align: center;
-    position: relative;
-    transition: all .3s ease-in-out;
-    bottom: 0;
-    img {
-      margin: 66px auto 60px;
-    }
-    h3 {
-      margin: 0;
-      font-size: 18px;
-      color: #1f2f3d;
-      font-weight: normal;
-    }
-    p {
-      font-size: 14px;
-      color: #99a9bf;
-      padding: 0 25px;
-      line-height: 1.8;
-    }
-    a {
-      height: 53px;
-      line-height: 52px;
-      font-size: 14px;
-      color: #20a0ff;
-      text-align: center;
-      border: 0;
-      border-top: 1px solid #eaeefb;
-      padding: 0;
-      cursor: pointer;
-      width: 100%;
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      background-color: #fff;
-      border-radius: 0 0 5px 5px;
-      transition: all .3s;
-      text-decoration: none;
-      display: block;
-      &:hover {
-        color: #fff;
-        background: #20a0ff;
-      }
-    }
-    &:hover {
-      bottom: 6px;
-      box-shadow: 0px 6px 18px 0px rgba(232,237,250,0.50);
-    }
-  }
   .el-row {
     margin-bottom: 10px;
     &:last-child {
@@ -216,13 +116,7 @@
     from { opacity: 0; }
     to { opacity: 1; }
   }
-  @media (max-width: 1140px) {
-    .cards {
-      width: 100%;
-      .container {
-        width: 100%;
-      }
-    }
+  @media (max-width: 1440px) {
     .banner .container {
       width: 100%;
       box-sizing: border-box;
@@ -239,17 +133,6 @@
     }
   }
   @media (max-width: 768px) {
-    .cards {
-      li {
-        width: 80%;
-        margin: 0 auto 20px;
-        float: none;
-      }
-      .card {
-        height: auto;
-        padding-bottom: 54px;
-      }
-    }
     .banner-stars {
       display: none;
     }
@@ -268,7 +151,6 @@
 </style>
 <script>
 import Vue from 'vue'
-import axios from 'axios'
 import theaterJS from 'theaterjs'
 import Component, { Getter } from 'class-component'
 
@@ -276,20 +158,6 @@ import Component, { Getter } from 'class-component'
 export default class Home extends Vue {
   @Getter authUser
 
-  user = {
-    userName: '',
-    password: '',
-    captcha: ''
-  }
-  rules = {}
-  captchaSvg = ''
-  logining = false
-  beforeMount () {
-    axios.get('/hpi/captcha')
-    .then((res) => {
-      this.captchaSvg = res.data
-    })
-  }
   mounted () {
     function typing (theater) {
       theater
@@ -322,26 +190,10 @@ export default class Home extends Vue {
         done()
       })
   }
-  login () {
-    this.logining = true
-    setTimeout(function () {
-      this.logining = false
-    }.bind(this), 1000)
-    this.$refs.user.validate((valid) => {
-      if (valid) {
-        this.$store.dispatch('login', this.user)
-        .then(() => {
-          this.user.userName = ''
-          this.user.password = ''
-        })
-        .catch((e) => {
-          this.$message.warning(e.message)
-        })
-      }
-    })
-  }
   logout () {
-    this.$store.dispatch('logout')
+    this.$store.dispatch('logout', () => {
+      this.$router.push('/login')
+    })
   }
 }
 </script>
