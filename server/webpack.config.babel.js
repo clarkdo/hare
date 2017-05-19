9ea2459915d9a12f331f87dd8021515a6351d9a4
clarkdo <clark.duxin@gmail.com>
2017-05-19 11:15:07 +0800

    fix unable to login issue

--- server/webpack.config.babel.js
+++ server/webpack.config.babel.js
@@ -1,7 +1,6 @@
 import webpack from 'webpack'
 import nodeExternals from 'webpack-node-externals'
 import ProgressBarPlugin from 'progress-bar-webpack-plugin'
-const nodeEnv = 'production'
 
 export default {
   entry: {
@@ -22,7 +21,7 @@ export default {
   ],
   plugins: [
     new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new ProgressBarPlugin()
   ]
