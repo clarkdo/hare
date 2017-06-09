46dcab83d7d2ead516b4e16fdde25d01d3ed14f3
Clark Du <duxin@cfilmcloud.com>
2017-06-09 17:56:00 +0800

    improve unit tests

---
+++ test/config.test.js
@@ -0,0 +1,53 @@
+import test from 'ava'
+import Nuxt from 'nuxt'
+import { resolve } from 'path'
+
+const port = 4000
+// const url = (route) => 'http://localhost:' + port + route
+
+let nuxt = null
+let server = null
+
+const wp = p => /^win/.test(process.platform) ? p.replace(/[\\/]/g, '\\\\') : p
+
+// Init nuxt.js and create server listening on localhost:4000
+test.before('Init Nuxt.js', async t => {
+  const rootDir = resolve(__dirname, '..')
+  let config = require(resolve(rootDir, 'nuxt.config.js'))
+  config.rootDir = rootDir
+  config.dev = false
+  nuxt = new Nuxt(config)
+  await nuxt.build()
+  server = new nuxt.Server(nuxt)
+  server.listen(port, 'localhost')
+})
+
+test('Vendor', async t => {
+  const vendor = nuxt.options.build.vendor
+  t.true(!!~vendor.indexOf('axios'), 'axios added to config')
+  t.true(!!~vendor.indexOf('element-ui'), 'element-ui added to config')
+  t.true(!!~vendor.indexOf('vue-class-component'), 'vue-class-component added to config')
+  t.true(!!~vendor.indexOf('vuex-class'), 'vuex-class added to config')
+  t.true(!!~vendor.indexOf('lodash/debounce'), 'lodash/debounce added to config')
+})
+
+test('Plugin', async t => {
+  const plugins = nuxt.options.plugins
+  t.is(plugins[0], wp('~plugins/element-ui'), 'element-ui plugin added to config')
+  t.is(plugins[1].src, wp('~plugins/auth-header'), 'auth-header plugin added to config')
+  t.is(plugins[1].ssr, false, 'auth-header plugin ssr is false')
+})
+
+test('Middleware', async t => {
+  const { html, redirected } = await nuxt.renderRoute('/', {req: {}})
+  t.true(html.includes('<div id="__nuxt"></div>'), 'auth plugin works 1')
+  t.true(!html.includes('前端项目模板'), 'auth plugin works 2')
+  t.true(redirected.path === '/login', 'auth plugin works 3')
+  t.true(redirected.status === 302, 'auth plugin works')
+})
+
+// Close server and ask nuxt to stop listening to file changes
+test.after('Closing server and nuxt.js', t => {
+  server.close()
+  nuxt.close()
+})
---
+++ test/login.test.js
@@ -0,0 +1,32 @@
+import test from 'ava'
+import Nuxt from 'nuxt'
+import { resolve } from 'path'
+
+// We keep the nuxt and server instance
+// So we can close them at the end of the test
+let nuxt = null
+let server = null
+
+// Init Nuxt.js and create a server listening on localhost:4000
+test.before('Init Nuxt.js', async t => {
+  const rootDir = resolve(__dirname, '..')
+  let config = require(resolve(rootDir, 'nuxt.config.js'))
+  config.rootDir = rootDir // project folder
+  config.dev = false // production build
+  nuxt = new Nuxt(config)
+  await nuxt.build()
+  server = new nuxt.Server(nuxt)
+  server.listen(4000, 'localhost')
+})
+
+test('Route /login', async t => {
+  const { html } = await nuxt.renderRoute('/login', {req: {session: {}}})
+  t.true(html.includes('placeholder="请输入用户名"'))
+  t.true(html.includes('placeholder="请输入密码"'))
+})
+
+// Close server and ask nuxt to stop listening to file changes
+test.after('Closing server and nuxt.js', t => {
+  server.close()
+  nuxt.close()
+})
