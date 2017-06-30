d1077a59472ef64c1aafd065418b989a860c6083
clarkdo <clark.duxin@gmail.com>
2017-06-30 15:22:42 +0800

    add doughnut and pie chart demo

---
+++ client/components/charts/DoughnutDemo.vue
@@ -0,0 +1,24 @@
+<script>
+import Component from 'class-component'
+import { Doughnut } from 'vue-chartjs'
+
+@Component
+export default class DoughnutDemo extends Doughnut {
+  mounted () {
+    this.renderChart({
+      labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+      datasets: [
+        {
+          backgroundColor: [
+            '#41b883',
+            '#ffcd56',
+            '#20a0ff',
+            '#f87979'
+          ],
+          data: [40, 20, 80, 10]
+        }
+      ]
+    })
+  }
+}
+</script>
