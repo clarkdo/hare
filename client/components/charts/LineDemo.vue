046a7520db28d21d057883ec619148d4fdf532bf
clarkdo <clark.duxin@gmail.com>
2017-06-30 16:54:25 +0800

    add axes in line chart demo

--- client/components/charts/LineDemo.vue
+++ client/components/charts/LineDemo.vue
@@ -4,26 +4,27 @@ import { Line } from 'vue-chartjs'
 
 @Component
 export default class LineDemo extends Line {
+  chartData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'Data One',
+        fill: false,
+        borderColor: '#20a0ff',
+        backgroundColor: '#20a0ff',
+        data: [40, 39, 10, 40, 39, 80, 40]
+      },
+      {
+        label: 'Data Two',
+        fill: false,
+        borderColor: '#f87979',
+        backgroundColor: '#f87979',
+        data: [37, 14, 49, 28, 92, 58, 51]
+      }
+    ]
+  }
   mounted () {
-    this.renderChart({
-      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-      datasets: [
-        {
-          label: 'Data One',
-          fill: false,
-          borderColor: '#20a0ff',
-          backgroundColor: '#20a0ff',
-          data: [40, 39, 10, 40, 39, 80, 40]
-        },
-        {
-          label: 'Data Two',
-          fill: false,
-          borderColor: '#f87979',
-          backgroundColor: '#f87979',
-          data: [37, 14, 49, 28, 92, 58, 51]
-        }
-      ]
-    }, {
+    this.renderChart(this.chartData, {
       tooltips: {
         callbacks: {
           label: (tooltipItem, data) => {
@@ -32,8 +33,31 @@ export default class LineDemo extends Line {
             return 'Data is: ' + desc
           }
         }
+      },
+      scales: {
+        xAxes: [{
+          gridLines: {
+            display: true,
+            drawTicks: false
+          }
+        }],
+        yAxes: [{
+          gridLines: {
+            display: true,
+            drawTicks: false
+          }
+        }]
       }
     })
+    // setInterval(() => {
+    //   this.chartData.labels.push(this.chartData.labels[0])
+    //   this.chartData.datasets[0].data.push((Math.random() * 100).toFixed())
+    //   this.chartData.datasets[1].data.push((Math.random() * 100).toFixed())
+    //   this.chartData.labels.shift()
+    //   this.chartData.datasets[0].data.shift()
+    //   this.chartData.datasets[1].data.shift()
+    //   this._chart.update()
+    // }, 1000)
   }
 }
 </script>
