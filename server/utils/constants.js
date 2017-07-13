3bc346b04b6fb0354bc28828c737cdf8113864c9
Clark Du <duxin@cfilmcloud.com>
2017-07-13 12:47:15 +0800

    add client constants

--- server/utils/constants.js
+++
@@ -1,9 +0,0 @@
-const PORT = process.env.PORT || '3000'
-
-export default Object.freeze({
-  API: 'hpi',
-  BASE_API: '/hpi',
-  HOST: process.env.HOST || '127.0.0.1',
-  PORT,
-  LB_ADDR: process.env.LB_ADDR || `http://localhost:${PORT}/hpi`
-})
