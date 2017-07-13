0c39aaacb9450b4a4a8903c7430500d1ffe84814
clarkdo <clark.duxin@gmail.com>
2017-07-13 12:16:02 +0800

    refactor promise to async

--- client/store/index.js
+++ client/store/index.js
@@ -36,32 +36,27 @@ export const getters = {
 export const actions = {
   nuxtServerInit ({ commit }, { req }) {
   },
-  login ({ commit }, { userName, password, captcha }) {
-    return axios.post('/hpi/login', {
-      userName,
-      password,
-      captcha
-    })
-      .then((res) => {
-        let token = res.data['access_token']
-        setToken(token)
-        commit('SET_USER', getUserFromToken(token))
-      })
-      .catch((error) => {
-        let message = error.message
-        if (error.response.data) {
-          message = (error.response.data.message || message)
-        }
-        throw new Error(message)
-      })
+  async login ({ commit }, { userName, password, captcha }) {
+    try {
+      const {data: {access_token: token}} = await axios.post(
+        '/hpi/login',
+        {userName, password, captcha}
+      )
+      setToken(token)
+      commit('SET_USER', getUserFromToken(token))
+    } catch (error) {
+      let message = error.message
+      if (error.response.data) {
+        message = (error.response.data.message || message)
+      }
+      throw new Error(message)
+    }
   },
-  logout ({ commit }, callback) {
-    return axios.post('/hpi/logout')
-      .then(() => {
-        commit('SET_USER', null)
-        unsetToken()
-        callback()
-      })
+  async logout ({ commit }, callback) {
+    await axios.post('/hpi/logout')
+    commit('SET_USER', null)
+    unsetToken()
+    callback()
   },
   toggleMenu ({ commit }) {
     commit('SET_MENU_HIDDEN')
