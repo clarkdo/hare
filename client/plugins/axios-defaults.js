2864b48725709fb4805d598442dd8ccee46cd3f4
clarkdo <clark.duxin@gmail.com>
2017-07-25 11:52:39 +0800

    add axios interceptors for 401 error

--- client/plugins/axios-defaults.js
+++ client/plugins/axios-defaults.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
-import { setAuthHeader } from '~/utils/auth'
+import { setAuthHeader, getUserFromLocalStorage } from '~/utils/auth'
 
 const PORT = process.env.PORT || '3000'
 
-export default ({ req, isDev, isServer }) => {
+export default ({ req, isDev, isServer, route, redirect }) => {
   if (!isServer) {
     setAuthHeader(req)
+    if (!getUserFromLocalStorage() && route.name !== 'login') {
+      redirect('/login', { page: route.fullPath })
+    }
   }
   axios.defaults.timeout = 5000
   // for generate
