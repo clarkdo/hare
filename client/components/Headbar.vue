90b73412589110ddaeadc9eefc36376b5f5a848f
clarkdo <clark.duxin@gmail.com>
2017-08-18 15:34:14 +0800

    adjust position of navgation icon

--- client/components/Headbar.vue
+++ client/components/Headbar.vue
@@ -3,7 +3,7 @@
     <header class="header" ref="header">
       <el-row>
         <el-col :span="5">
-          <div id="nav-icon" :class="{open: !isMenuHidden}" @click="toggleMenu">
+          <div class="nav-icon" :class="{hide: !isMenuHidden}" @click="toggleMenu">
             <span></span>
             <span></span>
             <span></span>
@@ -67,58 +67,6 @@ export default class Headbar extends Vue {
     @media (max-width: 768px) {
       .el-col p span {
         display: none;
-      }
-    }
-    /* Nav Icon */
-    #nav-icon {
-      $first-top: 5px;
-      $space: 8px;
-      width: 27px;
-      height: 30px;
-      position: relative;
-      margin: 15px 5px;
-      transform: rotate(0deg);
-      transition: .5s ease-in-out;
-      cursor: pointer;
-      span {
-        display: block;
-        position: absolute;
-        height: 2px;
-        width: 100%;
-        background: #324157;
-        border-radius: 9px;
-        opacity: 1;
-        left: 0;
-        transform: rotate(0deg);
-        transition: .25s ease-in-out;
-        &:nth-child(1) {
-          top: $first-top;
-        }
-        &:nth-child(2),
-        &:nth-child(3) {
-          top: $first-top + $space;
-        }
-        &:nth-child(4) {
-          top: $first-top + $space * 2;
-        }
-      }
-      &.open span {
-        &:nth-child(1) {
-          top: $first-top + $space;
-          width: 0%;
-          left: 50%;
-        }
-        &:nth-child(2) {
-          transform: rotate(45deg);
-        }
-        &:nth-child(3) {
-          transform: rotate(-45deg);
-        }
-        &:nth-child(4) {
-          top: $first-top + $space;
-          width: 0%;
-          left: 50%;
-        }
       }
     }
     .el-col:nth-child(2),
