4463ccbbb5dfbf16745095a38b88c36df586ad46
Clark Du <clark.du@betbright.com>
2018-05-23 11:03:53 +0100

    misc: change ava helpers to cjs before profile.js supports transpiling

--- test/helpers/create-nuxt.js
+++ test/helpers/create-nuxt.js
@@ -1,7 +1,10 @@
-import { Nuxt } from 'nuxt'
-import { resolve } from 'path'
+// import { Nuxt } from 'nuxt'
+// import { resolve } from 'path'
+const { Nuxt } = require('nuxt')
+const { resolve } = require('path')
 
-export default function createNuxt () {
+// export default function createNuxt () {
+module.exports = function createNuxt () {
   const rootDir = resolve(__dirname, '../../')
   let config = require(resolve(rootDir, 'nuxt.config.js'))
   config.rootDir = rootDir // project folder
