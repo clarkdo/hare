d20e78ab35aa50d393af2ec9c14b24d4d92d1480
Clark Du <clark.duxin@gmail.com>
2019-04-15 13:10:02 +0100

    refactor(server): extract rebots server middleware

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -6,26 +6,19 @@ const consts = require('../utils/consts')
 const content = require('./content')
 const examples = require('./errors')
 const responseTime = require('./response-time')
+const robots = require('./response-time')
 
 module.exports = (app) => {
   // Add valid and beforeSave hooks here to ensure session is valid #TODO
   const SESSION_CONFIG = {
     key: consts.SESS_KEY
   }
-
   // session for flash messages (uses signed session cookies, with no server storage)
   app.use(session(SESSION_CONFIG, app))
-
   app.use(responseTime)
   // HTTP compression
   app.use(compress({}))
-  // only search-index www subdomain
-  app.use(async function robots(ctx, next) {
-    await next()
-    if (ctx.hostname.slice(0, 3) !== 'www') {
-      ctx.response.set('X-Robots-Tag', 'noindex, nofollow')
-    }
-  })
+  app.use(robots)
   // parse request body into ctx.request.body
   app.use(body())
   app.use(content)
